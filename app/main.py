5e41add999876f83c8e380ce6999201f9b89cdd5
Paul Zakharov <pasha-zakharov-14@mail.ru>
2024-04-15 12:42:51 +0400

    task8

--- app/main.py
+++ app/main.py
@@ -31,8 +31,8 @@ app.include_router(r_6)
 # from app.routes.task_7 import router as r_7
 # app.include_router(r_7)
 #
-# from app.routes.task_8 import router as r_8
-# app.include_router(r_8)
+from app.routes.task_8 import router as r_8
+app.include_router(r_8)
 #
 # from app.routes.task_11 import router as r_11
 # app.include_router(r_11)
--- app/routes/task_8.py
+++ app/routes/task_8.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Response
+import json
+from functools import wraps
+from fastapi import APIRouter
 
 router = APIRouter(tags=["Стажировка"])
 
@@ -9,12 +11,40 @@ router = APIRouter(tags=["Стажировка"])
 Оберните роут new_request() этим декоратором.
 Подумать, как хранить переменную с кол-вом сделаных запросов.
 """
-def count_requests():
-    pass
+
+file_count_path = 'app/files/task8/request_count.json'
+
+
+def load_count():
+    try:
+        with open(file_count_path, "r") as file:
+            data = json.load(file)
+            return data["count"]
+    except FileNotFoundError:
+        return 0
+
+
+def save_count(count):
+    data = {"count": count}
+    with open(file_count_path, "w") as file:
+        json.dump(data, file)
+
+
+def count_requests(func):
+    func.count = load_count()
+
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        func.count += 1
+        print(f"Количество запросов: {func.count}")
+        save_count(func.count)
+        return await func(*args, **kwargs)
+
+    return wrapper
 
 
 @router.get("/new_request", description="Задание_8. Декоратор - счётчик запросов.")
+@count_requests
 async def new_request():
     """Возвращает кол-во сделанных запросов."""
-
-    return Response()
+    return {'количество запросов': load_count()}
